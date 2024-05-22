dda56af9137f6f97f47d3fa5dc37ef4a601e7bf5
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-22 14:00:53 +0200

    Header en md: y mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
 export const Header = () => {
     return (
-        <header className="h-16 bg-zinc-800 md:bg-gray-100 flex items-center md:justify-end px-16 text-lg">
-            <ul id="menu-list" className="md:flex md:h-full md:flex md:items-center md:place-content-around tracking-wider">
+        <header className="md:justify-end md:px-16 md:bg-gray-100 md:text-lg h-16 bg-zinc-800 flex items-center justify-between px-8">
+            <a href="#home" className="md:hidden text-4xl text-white">CT</a>
+            <button id="menu-button" className="md:hidden text-white text-4xl">
+                <i className="bi bi-list">AA</i>
+            </button>
+            <ul id="menu-list" className="md:flex md:h-full md:flex md:items-center md:place-content-around tracking-wider hidden">
                 <li className="nav-item"><a href="#home">Inicio</a></li>
                 <li className="nav-item"><a href="#skills">Habilidades</a></li>
                 <li className="nav-item"><a href="#projects">Proyectos</a></li>
