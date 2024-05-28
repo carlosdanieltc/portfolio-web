5e207d7652881532c55e54efd20159efc4bd9e0a
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-28 14:03:53 +0200

    Creado Componente Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Home } from "./components/Home"
+import { Projects } from "./components/Projects"
 import { Skills } from "./components/Skills"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Home></Home>
       <Skills></Skills>
+      <Projects></Projects>
     </>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,8 @@
+export const Projects = () => {
+    return (
+        <div id="projects" className="bg-gray-100 flex flex-col md:py-28">
+            <h1 className="md:text-4xl text-3xl font-medium w-4/5 text-center border-b-2 mx-auto py-2 md:pb-6">Mis proyectos realizados</h1>
+            
+        </div>
+    )
+}
