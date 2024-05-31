ea4634c4a87f9f0967016a8c3e7fe3fd40286121
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-31 11:29:54 +0200

    Cambios estilos

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
 export const ProjectCard = ({ title, description, projectLink, gitHubLink, imgProject, technologies }) => {
     return (
         <>
-            <div className="md:w-96 h-104 mx-5 md:mt-16 mt-8 rounded-xl border-2 border-black shadow-gray-400 shadow-lg">
+            <div className="md:w-96 h-104 mx-5 mt-8 md:mt-0 rounded-xl border-2 border-black shadow-gray-400 shadow-lg">
                 <div className="w-full h-96 bg-primaryDark rounded-t-xl text-white">
                     <a href={gitHubLink} target="_blank"><img
                         src={imgProject} className="w-full h-48 rounded-t-xl" alt="GiffApp" /></a>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,11 +5,11 @@ export const Projects = () => {
     return (
         <div id="projects" className="bg-gray-100 flex flex-col md:py-28">
             <h1 className="md:text-4xl text-3xl font-medium w-4/5 text-center border-b-2 mx-auto py-2 md:pb-6">Mis proyectos realizados</h1>
-            <div className="w-full flex flex-col md:flex-row justify-center bg-blue-200">
+            <div className="py-12 w-full flex flex-col md:flex-row justify-center items-center">
 
                 <button className="bg-gray-600">Izquierda</button>
 
-                <div className="flex flex-col md:flex-row">
+                <div className="flex flex-col md:flex-row mx-4">
                     {projects.map((project, index) => (
                         <ProjectCard
                             key={index}
@@ -26,7 +26,7 @@ export const Projects = () => {
                 <button className="bg-gray-600">Derecha</button>
 
             </div>
-            <p className="text-center mx-4">Puedes ver todos mis proyectos en mi perfil de <a href="https://github.com/carlosdanieltc"
+            <p className="text-center mx-4 my-4">Puedes ver todos mis proyectos en mi perfil de <a href="https://github.com/carlosdanieltc"
                 target="_blank" className="text-blue-600 font-medium">GitHub</a>.</p>
         </div>
     )
