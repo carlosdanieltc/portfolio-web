f55d7b1f6bdddaeba4265d7038e0b60a7a01c12b
Carlos Tabares <carlosdanieltc@gmail.com>
2024-05-31 10:51:49 +0200

    Inicio apartado proyectos. Creación de Project Card y projectsList

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,22 @@
+import { ProjectCard } from "./ProjectCard"
+
 export const Projects = () => {
     return (
         <div id="projects" className="bg-gray-100 flex flex-col md:py-28">
             <h1 className="md:text-4xl text-3xl font-medium w-4/5 text-center border-b-2 mx-auto py-2 md:pb-6">Mis proyectos realizados</h1>
-            
+            <div className="w-full flex flex-col md:flex-row justify-center bg-blue-200">
+
+                <button className="bg-gray-600">Izquierda</button>
+
+                <div className="flex flex-col md:flex-row">
+                    <ProjectCard></ProjectCard>
+                </div>
+
+                <button className="bg-gray-600">Derecha</button>
+
+            </div>
+            <p className="text-center mx-4">Puedes ver todos mis proyectos en mi perfil de <a href="https://github.com/carlosdanieltc"
+                target="_blank" className="text-blue-600 font-medium">GitHub</a>.</p>
         </div>
     )
 }
---
+++ src/components/projectsList.js
@@ -0,0 +1,32 @@
+export const projects = [ 
+    {
+        title: 'GifsApp',
+        description: 'Proyecto realizado para la práctica de React, en el cuál conectamos con la API de Giphy y podemos realizar búsquedas de gifs de todo tipo.',
+        projectLink: 'https://carlosdanieltc.github.io/react-gif-expert-/',
+        gitHubProjectLink: 'https://github.com/carlosdanieltc/react-gif-expert-',
+        imgProject: '',
+        technologies: [
+            
+        ]
+    },
+    {
+        title: '',
+        description: '',
+        projectLink: '',
+        gitHubProjectLink: '',
+        imgProject: '',
+        technologies: [
+            
+        ]
+    },
+    {
+        title: '',
+        description: '',
+        projectLink: '',
+        gitHubProjectLink: '',
+        imgProject: '',
+        technologies: [
+            
+        ]
+    },
+]
