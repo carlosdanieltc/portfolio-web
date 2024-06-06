a7c02b5854f035b3c33d4bbc7892e62a338b5e5d
Carlos Tabares <carlosdanieltc@gmail.com>
2024-06-06 12:00:20 +0200

    Ajustes estilos Skills para portatil

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,26 +4,26 @@ import { herramientasImg } from './imgTech'
 export const Skills = () => {
 
     return (
-        <div id="skills" className="bg-gray-100 md:pt-28 pb-12 pt-16 dark:text-white transition duration-300">
-            <h1 className="md:text-4xl text-3xl font-medium w-4/5 text-center border-b-2 mx-auto py-2 md:pb-6">Habilidades</h1>
-            <div className="p-1 flex h-5/6 md:px-16 flex-col md:flex-row">
+        <div id="skills" className="bg-gray-100 2xl:pt-28 pb-12 pt-16 dark:text-white transition duration-300">
+            <h1 className="2xl:text-4xl text-3xl font-medium w-4/5 text-center border-b-2 mx-auto py-2 md:pb-6">Habilidades</h1>
+            <div className="p-1 flex h-5/6 md:px-16 flex-col md:flex-row gap-4">
                 <div className="md:w-1/2 flex flex-col justify-start pt-6 md:pt-10">
-                    <h3 className="text-center md:text-3xl text-2xl font-medium py-2 mb-5">Tecnologías</h3>
-                    <ul className="flex flex-wrap justify-center items-center md:mr-6">
+                    <h3 className="text-center 2xl:text-3xl text-2xl font-medium 2xl:py-2 mb-5">Tecnologías</h3>
+                    <ul className="flex flex-wrap justify-center items-center 2xl:mr-6">
                         {Object.keys(tecnologiasImg).map((tech, index) => (
                             <li key={index} className='skill-option'>
-                                <img className='w-1/2 mb-2' src={tecnologiasImg[tech].img} alt={tecnologiasImg[tech].title} />
+                                <img className='w-1/3 2xl:w-1/2 mb-2' src={tecnologiasImg[tech].img} alt={tecnologiasImg[tech].title} />
                                 <h4>{tecnologiasImg[tech].title}</h4>
                             </li>
                         ))}
                     </ul>
                 </div>
                 <div className="md:w-1/2 flex flex-col justify-start pt-6 md:pt-10">
-                    <h3 className="text-center md:text-3xl text-2xl font-medium py-2 mb-5">Herramientas</h3>
-                    <ul className="flex flex-wrap justify-center items-center md:mr-6">
+                    <h3 className="text-center 2xl:text-3xl text-2xl font-medium 2xl:py-2 mb-5">Herramientas</h3>
+                    <ul className="flex flex-wrap justify-center items-center 2xl:mr-6">
                         {Object.keys(herramientasImg).map((tech, index) => (
                             <li key={index} className='skill-option'>
-                                <img className='w-1/2 mb-2' src={herramientasImg[tech].img} alt={herramientasImg[tech].title} />
+                                <img className='w-1/3 2xl:w-1/2 mb-2' src={herramientasImg[tech].img} alt={herramientasImg[tech].title} />
                                 <h4>{herramientasImg[tech].title}</h4>
                             </li>
                         ))}
