8ef5e6765095dbee9a83f0f3865e8080f5feaeae
Carlos Tabares <carlosdanieltc@gmail.com>
2024-06-28 12:03:18 +0200

    Enrutado img

--- src/components/imgTech.js
+++ src/components/imgTech.js
@@ -1,94 +1,92 @@
 export const tecnologiasImg = {
     react: {
-        "img": '../../public/assets/technologies/react.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/react.png`,
         "title": "React"
     },
     javaScript: {
-        "img": '../../public/assets/technologies/javascript.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/javascript.png`,
         "title": "JavaScript"
     },
     typeScript: {
-        "img": '../../public/assets/technologies/typescript.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/typescript.png`,
         "title": "TypeScript"
     },
     html: {
-        "img": '../../public/assets/technologies/html.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/html.png`,
         "title": "HTML"
     },
     css: {
-        "img": '../../public/assets/technologies/css.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/css.png`,
         "title": "CSS"
     },
     tailwind: {
-        "img": '../../public/assets/technologies/tailwind.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/tailwind.png`,
         "title": "Tailwind"
     },
     sass: {
-        "img": '../../public/assets/technologies/sass.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/sass.png`,
         "title": "Sass"
     },
     java: {
-        "img": '../../public/assets/technologies/java.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/java.png`,
         "title": "Java"
     },
     mysql: {
-        "img": '../../public/assets/technologies/mysql.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/mysql.png`,
         "title": "MySQL"
     },
     liteElement: {
-        "img": '../../public/assets/technologies/lit.png',
+        "img": `${import.meta.env.BASE_URL}assets/technologies/lit.png`,
         "title": "LitElement"
     }
-}
+};
 
 export const herramientasImg = {
     visualStudio: {
-        "img": '../../public/assets/myTools/visual_studio_code.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/visual_studio_code.png`,
         "title": "Visual Studio"
     },
     git: {
-        "img": '../../public/assets/myTools/git.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/git.png`,
         "title": "Git"
     },
     heidiSQL: {
-        "img": '../../public/assets/myTools/heidisql.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/heidisql.png`,
         "title": "HeidiSQL"
     },
     postman: {
-        "img": '../../public/assets/myTools/postman.webp',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/postman.webp`,
         "title": "Postman"
     },
     sourceTree: {
-        "img": '../../public/assets/myTools/sourcetree.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/sourcetree.png`,
         "title": "SourceTree"
     },
     agile: {
-        "img": '../../public/assets/myTools/agile.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/agile.png`,
         "title": "Agile"
     },
     bootstrap: {
-        "img": '../../public/assets/myTools/bootstrap.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/bootstrap.png`,
         "title": "Bootstrap"
     },
     vite: {
-        "img": '../../public/assets/myTools/vite.png',
+        "img": `${import.meta.env.BASE_URL}assets/myTools/vite.png`,
         "title": "Vite"
     },
-}
+};
 
 export const proyectosImg = () => [
     {
-        "img": '../../public/assets/myProjects/GifApp.png',
+        "img": `${import.meta.env.BASE_URL}assets/myProjects/GifApp.png`,
         "title": "GifsApp"
     },
     {
-        "img": '../../public/assets/myProjects/TodoApp.png',
+        "img": `${import.meta.env.BASE_URL}assets/myProjects/TodoApp.png`,
         "title": "ToDoList"
     },
     {
-        "img": '../../public/assets/myProjects/bbva2.png',
+        "img": `${import.meta.env.BASE_URL}assets/myProjects/bbva2.png`,
         "title": "BBVA"
     },
-]
-
-
+];
--- src/components/projectsList.js
+++ src/components/projectsList.js
@@ -1,4 +1,4 @@
-import { tecnologiasImg, herramientasImg } from './imgTech'
+import { tecnologiasImg, herramientasImg } from './imgTech';
 
 export const projects = [ 
     {
@@ -6,7 +6,7 @@ export const projects = [
         description: 'Proyecto realizado para la práctica de React, en el cuál conectamos con la API de Giphy y podemos realizar búsquedas de gifs de todo tipo.',
         projectLink: 'https://carlosdanieltc.github.io/react-gif-expert-/',
         gitHubLink: 'https://github.com/carlosdanieltc/react-gif-expert-',
-        imgProject: '../../public/assets/myProjects/GifsApp.png',
+        imgProject: `${import.meta.env.BASE_URL}assets/myProjects/GifsApp.png`,
         technologies: [
             tecnologiasImg.react, 
             tecnologiasImg.html,
@@ -20,7 +20,7 @@ export const projects = [
         description: 'Clásica TodoList realizada con React, donde además guardaremos en el LocalStorage los datos que hayamos introducido.',
         projectLink: 'https://carlosdanieltc.github.io/todoList/',
         gitHubLink: 'https://github.com/carlosdanieltc/todoList',
-        imgProject: '../../public/assets/myProjects/TodoApp.png',
+        imgProject: `${import.meta.env.BASE_URL}assets/myProjects/TodoApp.png`,
         technologies: [
             tecnologiasImg.react, 
             tecnologiasImg.html,
@@ -34,7 +34,7 @@ export const projects = [
         description: 'Página web realizada para la práctica de LitElement en la que veremos un ejemplo de ventana de inicio de bbva.',
         projectLink: 'https://carlosdanieltc.github.io/bbva-home/',
         gitHubLink: 'https://github.com/carlosdanieltc/bbva-home',
-        imgProject: '../../public/assets/myProjects/BBVA.png',
+        imgProject: `${import.meta.env.BASE_URL}assets/myProjects/BBVA.png`,
         technologies: [
             tecnologiasImg.liteElement,
             tecnologiasImg.html,
@@ -47,7 +47,7 @@ export const projects = [
         description: 'Proyecto realizado para la práctica de React del clásico juego Tic Tac Toe.',
         projectLink: 'https://carlosdanieltc.github.io/react-tic-tac-toe/',
         gitHubLink: 'https://github.com/carlosdanieltc/react-tic-tac-toe',
-        imgProject: '../../public/assets/myProjects/TicTacToe.png',
+        imgProject: `${import.meta.env.BASE_URL}assets/myProjects/TicTacToe.png`,
         technologies: [
             tecnologiasImg.react, 
             tecnologiasImg.html,
@@ -61,7 +61,7 @@ export const projects = [
         description: 'Buscador de películas realizado con React, conectado con la API de OMDb.',
         projectLink: 'https://carlosdanieltc.github.io/react-buscador-de-peliculas/',
         gitHubLink: 'https://github.com/carlosdanieltc/react-buscador-de-peliculas',
-        imgProject: '../../public/assets/myProjects/Peliculas.png',
+        imgProject: `${import.meta.env.BASE_URL}assets/myProjects/Peliculas.png`,
         technologies: [
             tecnologiasImg.react, 
             tecnologiasImg.html,
@@ -70,4 +70,4 @@ export const projects = [
             herramientasImg.vite
         ]
     },
-]
+];
