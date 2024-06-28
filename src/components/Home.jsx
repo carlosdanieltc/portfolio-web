1f5e7444bfc0ff4088aaaeb91affa6d31d7489af
Carlos Tabares <carlosdanieltc@gmail.com>
2024-06-28 11:23:04 +0200

    Prueba para enrutado de imagenes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export const Home = () => {
         <div id="home" className="md:bg-gray-100 md:h-screen md:pr-8 md:flex-row flex flex-col dark:text-white dark:bg-zinc-800">
             <div className="md:w-1/2 md:rounded-borderCustom md:z-50 md:pr-12 bg-zinc-800 flex flex-col items-center justify-center pt-16 md:pt-0">
                 <p className="md:ml-16 md:block mt-4 text-4xl 2xl:text-4xl text-white absolute top-0 left-0 hidden tracking-wider">CT</p>
-                <img src="./assets/CarlosAvatar.jpg" className="transition duration-300 dark:shadow-2xl dark:shadow-white  md:rounded-full 2xl:w-80 2xl:h-80 w-64 h-64 md:ml-auto" />
+                <img src="/assets/CarlosAvatar.jpg" className="transition duration-300 dark:shadow-2xl dark:shadow-white  md:rounded-full 2xl:w-80 2xl:h-80 w-64 h-64 md:ml-auto" />
                 <div className="md:absolute text-4xl 2xl:text-5xl my-3 w-[5.2rem] md:w-20 px-1 md:px-0 2xl:w-24 bottom-2 md:left-12 flex justify-between text-white">
                     <a href="https://www.linkedin.com/in/tabaresin" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
                     <a href="https://github.com/carlosdanieltc" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
